06c435444d35a5e2b3c67783e499c368cf2effad
VJ-21 <168296822+VJ-21@users.noreply.github.com>
2025-03-25 08:17:12 +0530

    Updated streamlit_app.py
    added streamlit title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+streamlit.title('My parents New Healthy Diner')
 
 # Write directly to the app
 st.title(" Customize Your Smoothie! :apple:")
