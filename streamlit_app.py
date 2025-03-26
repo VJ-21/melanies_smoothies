a5766f18c04b03b22331cc40928fcb03618e2cc3
VJ-21 <168296822+VJ-21@users.noreply.github.com>
2025-03-26 16:07:31 +0530

    Update streamlit_app.py
    brought down the first step

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+
 
 # Write directly to the app
 st.title(" Customize Your Smoothie! :apple:")
@@ -39,6 +38,9 @@ if ingredients_list:
 
     st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
+
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    st.text(smoothiefroot_response)
     
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
